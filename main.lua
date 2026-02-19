d9f8c5456a1691bca0c82808def5b0484a6abaee
CoDevs Studios <bintangrahmadantv112@gmail.com>
2026-02-19 14:50:04 +0700

    Create main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == (3851622790) then -- Break In Story Lobby
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KenzoSSto1/X-Hub-Lobby-Remakes/main/BreakInStoryLobby.lua", true))()
 elseif game.PlaceId == (4620170611) then -- Break In Story Main
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bebo-Mods/XHub/main/BreakInStoryMain.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KenzoSSto1/X-Hub-Main-Remakes/main/BreakInStoryMain.lua", true))()
 else
     game.StarterGui:SetCore(
         "SendNotification",
