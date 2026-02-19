b6e0f79bc58fe78a52a1e3ef5decc7de286e21a4
CoDevs Studios <bintangrahmadantv112@gmail.com>
2026-02-19 14:35:51 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,18 @@
+if game.PlaceId == (3851622790) then -- Break In Story Lobby
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bebo-Mods/XHub/main/BreakInStoryLobby.lua", true))()
+elseif game.PlaceId == (4620170611) then -- Break In Story Main
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bebo-Mods/XHub/main/BreakInStoryMain.lua", true))()
+else
+    game.StarterGui:SetCore(
+        "SendNotification",
+        {
+            Title = "Game Not Supported Yet",
+            Text = "Try Again Later"
+        }
+    )
+end
+
+
+-- Script by CoDevs Studios 
+-- not a copy script!
+-- Old Script
